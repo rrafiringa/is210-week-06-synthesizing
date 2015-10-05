7fcb66c36841629f5f8f322c8e24435f749dee8d
rrafiringa <rrafiringa@gmail.com>
2015-10-05 08:33:43 +0000

    pylint corrections added.

--- task_02.py
+++ task_02.py
@@ -12,12 +12,12 @@ def prepare_email(appointments):
     Returns:
         list: List of invites
     """
-    return ['Dear {},\nI look forward to meeting \
-with you on {}.\nBest,\nMe'.format(col[0], col[1]) \
-    for col in appointments]
+    return ['Dear {},\nI look forward to meeting with you \
+on {}.\nBest,\nMe'.format(col[0], col[1])
+            for col in appointments]
 
 
 if __name__ == '__main__':
-    for x in prepare_email([('Marlon','Monday, October 5, 2015 3:00PM'), \
-	('Sophia','Tuesday, October 6, 2015 at 11:00AM')]):
+    for x in prepare_email([('Marlon', 'Monday, October 5, 2015 3:00PM'),
+                            ('Sophia', 'Tuesday, October 6, 2015 at 11:00AM')]):
         print x
